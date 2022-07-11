2e2a5e023b366addd53fb985699080aeeb2053b1
John102020 <109115604+John102020@users.noreply.github.com>
2022-07-11 19:38:11 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
+from setuptools import setup
 
+setup(
+  name="Test",
+  author="John102020",
+  author_email="syugalvao@gmail.com",
+  license="MIT license",
+  description="This is my math library.",
+  packages=["DeepLearning"]
+)
